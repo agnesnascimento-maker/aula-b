030df49c9f21dd1c181896200fe20b4772f2114e
agnes.nascimento_maker <agnes.nascimento@edu.senai.br>
2026-09-18 11:19:45 -0300

    Meu primeiro commit

---
+++ conceito_tipos_de_dados.js
@@ -0,0 +1,32 @@
+// TIPOS DE DADOS
+//No javascript, tudo o que armazenamos em variáveis tem um tipo.Esses tipos definem oq podemos fazer com os valores.
+
+ // Principais tipos de dados:
+ // String(textos)
+ // Number (números)
+ // Boolean (verdadeiro ou falso)
+ // Object(Objetos, que agrupam informações)
+ // Array(Listas de valores)
+ // Null (Valor vazio)
+ // Underfined (quando algo não definido)
+
+ // STRING (Textos)
+ //Uma String é um texto, sempre escrito entre aspas("" ou '')
+ let nome = "Jarvis";
+ let mensagem ='Olá,Mundo!';
+
+ console.log(nome)
+ console.log(mensagem);
+
+ let saudacao= "Olá, "+ nome + "!";
+ console.log(saudacao);//Exibe "Olá, Jarvis"
+
+ //typeof
+ //O typeof serve para descobrir o tipo de uma valor o variável
+let nomeDois = "NicolY"
+console.log(typeof nomeDois); 
+
+
+let soma= 10 + 5
+
+
---
+++ exercicio_01.js
@@ -0,0 +1,12 @@
+//Atividade Nunber
+//crie ua variável chama anoAtual e outra chmada anoNascimento.
+//calcule e exiba a idade no(a) console.
+
+
+let anoAtual=2026
+let anoNascimento=2010
+
+let idade= anoAtual- anoNascimento
+console.idade;
+
+console.log("A idade é:" +idade);
