9d5f7e826adc8a0a906323619185d022cdc70a90
agnes.nascimento_maker <agnes.nascimento@edu.senai.br>
2026-09-18 11:24:20 -0300

    Corrige sintaxe e remove console.idade

--- exercicio_01.js
+++ exercicio_01.js
@@ -1,12 +1,10 @@
-//Atividade Nunber
-//crie ua variável chama anoAtual e outra chmada anoNascimento.
-//calcule e exiba a idade no(a) console.
+//Atividade Number
+//crie uma variável chamada anoAtual e outra chamada anoNascimento.
+//calcule e exiba a idade no console.
 
+let anoAtual = 2026;
+let anoNascimento = 2010;
 
-let anoAtual=2026
-let anoNascimento=2010
+let idade = anoAtual - anoNascimento;
 
-let idade= anoAtual- anoNascimento
-console.idade;
-
-console.log("A idade é:" +idade);
+console.log("A idade é: " + idade);
